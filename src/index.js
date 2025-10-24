7366111e52d81791955fadb561d804365b0f963d
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-10-24 19:47:48 +0500

    Add all the data before images

--- src/index.js
+++ src/index.js
@@ -53,13 +53,19 @@ async function getWeatherData(city){
 
 function displayWeatherInfo(data){
 
+    function getDayOfWeek(date) {
+        const dayOfWeek = new Date(date).getDay();    
+        return isNaN(dayOfWeek) ? null : 
+            ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][dayOfWeek];
+    }
+
     console.log(data)
 
     const {resolvedAddress: city,
-           days: [{conditions, temp, tempmax, tempmin, uvindex, windspeed, feelslike, pressure, humidity, sunrise, sunset}]} = data;
+           days: [{datetime, conditions, temp, tempmax, tempmin, uvindex, windspeed, feelslike, pressure, humidity, sunrise, sunset}, secondDay, thirdDay, forthDay, fifthDay, sixthDay, seventhDay, eigthDay]} = data;
     card.textContent = "";
     card.style.display = "flex";
-
+    
     // Containers for display data
     const cityTitleRow = document.createElement('div');
     cityTitleRow.classList.add('cityTitleRow');
@@ -107,7 +113,6 @@ function displayWeatherInfo(data){
     sunriseDisplay.textContent = sunrise;
     sunsetDisplay.textContent = sunset;
     windspeedDisplay.textContent = windspeed;
-    
     // Minor box wind
     const minorBoxWindDiv = document.createElement('div')
     const windIcon = document.createElement("i")
@@ -174,13 +179,113 @@ function displayWeatherInfo(data){
     minorConditionsRow.appendChild(minorBoxUvindex)
     minorConditionsRow.appendChild(minorBoxPressure)
 
+    // Forecast row
+    const forecastTitle = document.createElement('p')
+    forecastTitle.classList.add('forecastTitle')
+    forecastTitle.textContent = "7-Day Forecast"
+    card.appendChild(forecastTitle)
+
+    const weekForecastRow = document.createElement('div')
+    weekForecastRow.classList.add('weekForecastRow')
+    card.appendChild(weekForecastRow)
+
+    // Weekly forecast
+    const firstDayBox = document.createElement('div')
+    firstDayBox.classList.add('dayBox')
+    const firstDay = getDayOfWeek(datetime)
+    const firstDayBoxWeekday = document.createElement('p')
+    weekForecastRow.appendChild(firstDayBox)
+    firstDayBoxWeekday.textContent = "Today";
+    const firstDayHigh = document.createElement('p')
+    firstDayHigh.textContent = tempmax
+    const firstDayLow = document.createElement('p')
+    firstDayLow.textContent = tempmin
+    firstDayBox.append(firstDayBoxWeekday, firstDayHigh, firstDayLow)
+
+    const secondDayBox = document.createElement('div')
+    secondDayBox.classList.add('dayBox')
+    const secondDayOfTheWeek = getDayOfWeek(secondDay.datetime)
+    const secondDayBoxWeekday = document.createElement('p')
+    weekForecastRow.appendChild(secondDayBox)
+    secondDayBoxWeekday.textContent = secondDayOfTheWeek;
+    const secondDayHigh = document.createElement('p')
+    secondDayHigh.textContent = secondDay.tempmax
+    const secondDayLow = document.createElement('p')
+    secondDayLow.textContent = secondDay.tempmin
+    secondDayBox.append(secondDayBoxWeekday, secondDayHigh, secondDayLow)
+
+    const thirdDayBox = document.createElement('div')
+    thirdDayBox.classList.add('dayBox')
+    const thirdDayOfTheWeek = getDayOfWeek(thirdDay.datetime)
+    const thirdDayBoxWeekday = document.createElement('p')
+    weekForecastRow.appendChild(thirdDayBox)
+    thirdDayBoxWeekday.textContent = thirdDayOfTheWeek;
+    const thirdDayHigh = document.createElement('p')
+    thirdDayHigh.textContent = thirdDay.tempmax
+    const thirdDayLow = document.createElement('p')
+    thirdDayLow.textContent = thirdDay.tempmin
+    thirdDayBox.append(thirdDayBoxWeekday, thirdDayHigh, thirdDayLow)
+
+    const forthDayBox = document.createElement('div')
+    forthDayBox.classList.add('dayBox')
+    const forthDayOfTheWeek = getDayOfWeek(forthDay.datetime)
+    const forthDayBoxWeekday = document.createElement('p')
+    weekForecastRow.appendChild(forthDayBox)
+    forthDayBoxWeekday.textContent = forthDayOfTheWeek;
+    const forthDayHigh = document.createElement('p')
+    forthDayHigh.textContent = forthDay.tempmax
+    const forthDayLow = document.createElement('p')
+    forthDayLow.textContent = forthDay.tempmin
+    forthDayBox.append(forthDayBoxWeekday, forthDayHigh, forthDayLow)
+
+    const fifthDayBox = document.createElement('div')
+    fifthDayBox.classList.add('dayBox')
+    const fifthDayOfTheWeek = getDayOfWeek(fifthDay.datetime)
+    const fifthDayBoxWeekday = document.createElement('p')
+    weekForecastRow.appendChild(fifthDayBox)
+    fifthDayBoxWeekday.textContent = fifthDayOfTheWeek;
+    const fifthDayHigh = document.createElement('p')
+    fifthDayHigh.textContent = fifthDay.tempmax
+    const fifthDayLow = document.createElement('p')
+    fifthDayLow.textContent = fifthDay.tempmin
+    fifthDayBox.append(fifthDayBoxWeekday, fifthDayHigh, fifthDayLow)
+
+    const sixthDayBox = document.createElement('div')
+    sixthDayBox.classList.add('dayBox')
+    const sixthDayOfTheWeek = getDayOfWeek(sixthDay.datetime)
+    const sixthDayBoxWeekday = document.createElement('p')
+    weekForecastRow.appendChild(sixthDayBox)
+    sixthDayBoxWeekday.textContent = sixthDayOfTheWeek;
+    const sixthDayHigh = document.createElement('p')
+    sixthDayHigh.textContent = sixthDay.tempmax
+    const sixthDayLow = document.createElement('p')
+    sixthDayLow.textContent = sixthDay.tempmin
+    sixthDayBox.append(sixthDayBoxWeekday, sixthDayHigh, sixthDayLow)
+
+    const seventhDayBox = document.createElement('div')
+    seventhDayBox.classList.add('dayBox')
+    const seventhDayOfTheWeek = getDayOfWeek(seventhDay.datetime)
+    const seventhDayBoxWeekday = document.createElement('p')
+    weekForecastRow.appendChild(seventhDayBox)
+    seventhDayBoxWeekday.textContent = seventhDayOfTheWeek;
+    const seventhDayHigh = document.createElement('p')
+    seventhDayHigh.textContent = seventhDay.tempmax
+    const seventhDayLow = document.createElement('p')
+    seventhDayLow.textContent = seventhDay.tempmin
+    seventhDayBox.append(seventhDayBoxWeekday, seventhDayHigh, seventhDayLow)
+
+    const eigthDayBox = document.createElement('div')
+    eigthDayBox.classList.add('dayBox')
+    const eigthDayOfTheWeek = getDayOfWeek(eigthDay.datetime)
+    const eigthDayBoxWeekday = document.createElement('p')
+    weekForecastRow.appendChild(eigthDayBox)
+    eigthDayBoxWeekday.textContent = eigthDayOfTheWeek;
+    const eigthDayHigh = document.createElement('p')
+    eigthDayHigh.textContent = eigthDay.tempmax
+    const eigthDayLow = document.createElement('p')
+    eigthDayLow.textContent = eigthDay.tempmin
+    eigthDayBox.append(eigthDayBoxWeekday, eigthDayHigh, eigthDayLow)
     // card.appendChild(feelsLikeDisplay)
-    // card.appendChild(tempmaxDisplay)
-    // card.appendChild(tempminDisplay)
-    // card.appendChild(uvindexDisplay)
-    // card.appendChild(humudityDisplay)
-    // card.appendChild(pressureDisplay)
-    // card.
     // card.appendChild(sunriseDisplay)
     // card.appendChild(sunsetDisplay)
 
